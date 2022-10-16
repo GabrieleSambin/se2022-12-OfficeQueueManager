0f48f82dae40c8e81ad61c530987d093d1a63fa7
FabioTrovero <s301587@studenti.polito.it>
2022-10-16 18:43:06 +0200

    Counter Page First Release

--- client/src/views/Counter.jsx
+++ client/src/views/Counter.jsx
@@ -1,8 +1,43 @@
 //Imports
+import Form from 'react-bootstrap/Form';
+import { Button, Col, Row , Container} from "react-bootstrap";
 
 const Counter = () => {
     return (
-        <></>
+        <>
+        <Container className='h-100 d-flex flex-column align-item-center justify-content-center'>
+            <Row className='mb-5'>
+                <h2 className='mb-4 text-center'>Select a Cunter</h2>
+            </Row>
+            <Row>
+                <Col className='mt-1 text-center'>
+                        <Form>
+                            <div className="mb-3">
+                            <Form.Check
+                                inline
+                                label="Counter 1"
+                            />
+                            <Form.Check
+                                inline
+                                label="Counter 2"
+                            />
+                            <Form.Check
+                                inline
+                                label="Counter 3"
+                            />
+                            </div>
+                        </Form>
+                </Col>
+            </Row>
+            <Row>
+                <Col className='mt-3 text-center'>
+                    <Button className='mx-5 p-2'>
+                        Call Next
+                    </Button>
+                </Col>  
+            </Row>
+        </Container>
+        </>
     );
 }
 
