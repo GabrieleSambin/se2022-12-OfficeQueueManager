dcc62acbd2e5539805f4e2d72443ed0d0c725c16
adri-hevia <101174073+adri-hevia@users.noreply.github.com>
2022-10-18 21:47:41 +0200

    Date filtering in put api solved

--- server/README.md
+++ server/README.md
@@ -4,3 +4,9 @@ API:
 
 - PUT `/api/ticketState/:ID/:TDate`
     - param{ID, TDate}, body{counter, newState}
+
+- GET `/ServiceCounter`. Gets the services asociated to a counter id in the form of dictionaries within a list. e.g. [{ST_ID: 1}, {ST_ID: 3}]
+    - param{ID}
+
+- PUT `/api/Ticket`. Updates the Counter and State for a Date, Counter and Service. newState is 'CLOSE' and newID_Counter is ID_Counter.
+    - body{ST_ID, ID_Counter}
--- server/dao/TicketDAO.js
+++ server/dao/TicketDAO.js
@@ -24,16 +24,18 @@ exports.modifyTicket = (Ticket_Number, ST_ID, ID_Counter) => {
     return new Promise((resolve, reject) => {
         let newID_Counter = ID_Counter
         let newState = 'CLOSE'
+        let Today = dayjs().hour(0).minute(0).second(0)
+        console.log(Today)
         //Missing date filter --> tipo: AND DATE = TODAYS DAY
-        const sql1 = 'SELECT COUNT(*) AS count FROM Ticket WHERE Ticket_Number = ? AND ST_ID = ?';
-        db.get(sql1, [Ticket_Number, ST_ID], (err, r) => {
+        const sql1 = 'SELECT COUNT(*) AS count FROM Ticket WHERE Ticket_Number = ? AND ST_ID = ? AND Date >= ?';
+        db.get(sql1, [Ticket_Number, ST_ID, Today], (err, r) => {
             if (err) {
                 reject(err)
             } else if (r.count === 0) {
                 reject(new Error("ID not found"))
             } else {
-                const sql2 = 'UPDATE Ticket SET ID_Counter = ?, State = ? WHERE Ticket_Number = ? AND ST_ID = ?';
-                db.run(sql2, [newID_Counter, newState, Ticket_Number, ST_ID], (err) => {
+                const sql2 = 'UPDATE Ticket SET ID_Counter = ?, State = ? WHERE Ticket_Number = ? AND ST_ID = ? AND Date >= ?';
+                db.run(sql2, [newID_Counter, newState, Ticket_Number, ST_ID, Today], (err) => {
                     if (err) {
                         reject(err);
                     }
